a72c60006aa81dda31880ef15cfd96b7e573a6c4
ayunami2000 <spwilliamsiam@gmail.com>
2020-12-05 18:06:56 -0500

    Update finish-gui.sh

--- finish-gui.sh
+++ finish-gui.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-apt-get install -y anbox-modules-dkms android-tools-adb
+wget https://launchpad.net/~morphis/+archive/ubuntu/anbox-support/+files/anbox-modules-dkms_13_all.deb
+dpkg -i anbox-modules-dkms_13_all.deb
 modprobe ashmem_linux
 modprobe binder_linux
+apt-get install -y android-tools-adb
 wget https://redirector.gvt1.com/edgedl/android/studio/ide-zips/4.1.1.0/android-studio-ide-201.6953283-linux.tar.gz
 tar -xf android-studio-ide-201.6953283-linux.tar.gz
 cd android-studio/bin
