2e639a5992b68e6b98fdfaf93992227b73f79ca8
ayunami2000 <spwilliamsiam@gmail.com>
2020-12-05 17:55:19 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 set -ex
 
-RUN_FLUXBOX=${RUN_FLUXBOX:-yes}
-RUN_XTERM=${RUN_XTERM:-yes}
+#RUN_FLUXBOX=${RUN_FLUXBOX:-yes}
+#RUN_XTERM=${RUN_XTERM:-yes}
 
-case $RUN_FLUXBOX in
-  false|no|n|0)
-    rm -f /app/conf.d/fluxbox.conf
-    ;;
-esac
+#case $RUN_FLUXBOX in
+#  false|no|n|0)
+#    rm -f /app/conf.d/fluxbox.conf
+#    ;;
+#esac
 
-case $RUN_XTERM in
-  false|no|n|0)
-    rm -f /app/conf.d/xterm.conf
-    ;;
-esac
+#case $RUN_XTERM in
+#  false|no|n|0)
+#    rm -f /app/conf.d/xterm.conf
+#    ;;
+#esac
 
 exec supervisord -c /app/supervisord.conf
